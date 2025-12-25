8eb077aeb05bfb419f65c9520f58d812afc20688
Aurobindo Tripathy <aurobindotripathy@mac.lan>
2025-12-25 09:16:24 -0800

    turned google search built-in to a tool

--- nutri_agent/agent.py
+++ nutri_agent/agent.py
@@ -9,6 +9,12 @@ from google.adk.tools import google_search
 from google.adk.agents import LlmAgent
 import google.adk
 from pydantic import BaseModel, Field
+from .tools.get_nutriments_from_off import get_nutriments_from_off
+
+from google.adk.tools.google_search_tool import GoogleSearchTool
+
+# Initialize the tool with the bypass parameter set to True
+google_search_tool = GoogleSearchTool(bypass_multi_tools_limit=True)
 
 print(f'adk version: {google.adk.__version__}')
 model="gemini-2.0-flash"
@@ -22,7 +28,8 @@ ingredients_generator_agent = LlmAgent(
     name="IngredientsGenerator",
     model=model,
     instruction=load_instruction_from_file("ingredients_generator_instructions.txt"),
-    tools=[google_search],
+    # tools=[get_nutriments_from_off, google_search],
+    tools=[get_nutriments_from_off],
     output_schema=IngredientsListAndAilment,
     output_key="ingredients_list_and_ailment",  # Save result to state
 )
@@ -32,7 +39,7 @@ disease_identifier_agent = LlmAgent(
     name="DiseaseIdentifier",
     model=model,
     instruction=load_instruction_from_file("disease_identifier_instructions.txt"),
-    tools=[google_search],
+    tools=[google_search_tool, get_nutriments_from_off],
 )
 
 
