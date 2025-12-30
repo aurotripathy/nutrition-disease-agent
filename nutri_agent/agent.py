e18857fbf27c349a29384d8e890143672b8a1d29
Aurobindo Tripathy <aurobindotripathy@mac.lan>
2025-12-29 16:31:30 -0800

    tool name chnage

--- nutri_agent/agent.py
+++ nutri_agent/agent.py
@@ -10,14 +10,18 @@ from google.adk.agents import LlmAgent
 import google.adk
 from pydantic import BaseModel, Field
 from typing import Dict
-from .tools.get_nutriments_from_off import get_nutriments_from_off_grouped
+from .tools.get_nutriments_from_open_food_facts import get_grouped_nutriments_from_open_food_facts
+import logging
 
 from google.adk.tools.google_search_tool import GoogleSearchTool
+
+logging.basicConfig(level=logging.DEBUG)
 
 # Initialize the tool with the bypass parameter set to True
 google_search_wrapper = GoogleSearchTool(bypass_multi_tools_limit=True)
 
-print(f'adk version: {google.adk.__version__}')
+logging.debug(f'adk version: {google.adk.__version__}')
+
 model="gemini-2.5-pro"  # was flash
 # model="gemini-2.5-flash"  # was flash
 
@@ -32,7 +36,7 @@ ingredients_generator_agent = LlmAgent(
     model=model,
     instruction=load_instruction_from_file("ingredients_generator_instructions.txt"),
     # tools=[get_nutriments_from_off, google_search],
-    tools=[get_nutriments_from_off_grouped],
+    tools=[get_grouped_nutriments_from_open_food_facts],
     output_schema=IngredientsListAndAilment,
     output_key="ingredients_list_and_ailment",  # Save result to state
 )
